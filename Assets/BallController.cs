41783bac90a90e479b3d00bfea5f35d340dcc032
githir <githir@users.noreply.github.com>
2022-08-04 12:30:45 +0900

    add scoreText

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,9 +6,11 @@ public class BallController : MonoBehaviour
 {
     GameObject g;
     Rigidbody rb;
+    GameObject scoreText;
+
     float born;
     public Vector3 force = new Vector3(0.0f, 0.0f, 1.0f);    // óÕÇê›íË
-
+    private bool isGoal;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,9 @@ public class BallController : MonoBehaviour
         g = GameObject.Find("GameObject");
         rb = GetComponent<Rigidbody>();
         born = Time.time;
+        isGoal = false;
+
+        scoreText = GameObject.Find("ScoreText");
     }
 
     // Update is called once per frame
@@ -27,12 +32,39 @@ public class BallController : MonoBehaviour
         if (Time.time - born > 3.0f)
         {
             var velocity = rb.velocity;
-            if (velocity.sqrMagnitude < 0.01f) {
-                Debug.Log("STOP");
-                Destroy(this.gameObject);
-                g.GetComponent<TapTest>().numBalls -= 1;
+            if (velocity.sqrMagnitude < 0.01f)
+            {
+                //    Debug.Log("STOP");
+                if (this.isGoal == false)
+                {
+                    Destroy(this.gameObject);
+                    g.GetComponent<TapTest>().numBalls -= 1;
+                }
             }
-            Debug.Log(velocity.sqrMagnitude);
+          //  Debug.Log(velocity.sqrMagnitude);
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //GoalÇ…è’ìÀÇµÇΩèÍçá
+        if (other.gameObject.tag == "GoalTag")
+        {
+            Debug.Log("touch GoalCircle");
+            this.isGoal = true;
+            scoreText.GetComponent<ScoreTextController>().score += 1;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //GoalÇ…è’ìÀÇµÇΩèÍçá
+        if (other.gameObject.tag == "GoalTag")
+        {
+            Debug.Log("detouch GoalCircle");
+            this.isGoal = false;
+            scoreText.GetComponent<ScoreTextController>().score -= 1;
         }
     }
 }
--- Assets/TapTest.cs
+++ Assets/TapTest.cs
@@ -6,6 +6,7 @@ public class TapTest : MonoBehaviour
 {
     GameObject g;
     public GameObject ballPrefab;
+    private GameObject scoreText;
 
     private float spd_x_min = 0;
     private float spd_x_max = 0;
@@ -13,14 +14,22 @@ public class TapTest : MonoBehaviour
     private float spd_y_max = 0;
     private float spd_x = 0;
     private float spd_y = 0;
+    private float prev_x = 0;
+    private float lastThrough = 0;
 
     public int numBalls = 0;
-    public int maxNumBalls = 0;
+    public int maxNumBalls = 1;
+    public float throughInterval = 0.2f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         g = GameObject.Find("Sphere");
+        scoreText = GameObject.Find("ScoreText");
+
+            lastThrough = Time.time;
+    //    throughInterval = 0.2f;
     }
 
     // Update is called once per frame
@@ -33,39 +42,58 @@ public class TapTest : MonoBehaviour
             for (int i = 0; i < Input.touchCount; i++)
             {
                 Touch t = Input.GetTouch(i);
-                Debug.Log("t.altitudeAngle " + t.altitudeAngle);
+     /*
+      * Debug.Log("t.altitudeAngle " + t.altitudeAngle);
                 Debug.Log("t.azimuthAngle " + t.azimuthAngle);
                 Debug.Log("t.deltaPosition " + t.deltaPosition);
                 Debug.Log("t.deltaTime " + t.deltaTime);
+     */
+                spd_x = t.deltaPosition[1] / t.deltaTime;
+                spd_y = -t.deltaPosition[0] / t.deltaTime;
 
-                spd_x = t.deltaPosition[0] / t.deltaTime;
-                spd_y = t.deltaPosition[1] / t.deltaTime;
-                spd_x_min = (spd_x_min > spd_x) ? spd_x : spd_x_min;
-                spd_x_max = (spd_x_max < spd_x) ? spd_x : spd_x_max;
-                spd_y_min = (spd_y_min > spd_y) ? spd_y : spd_y_min;
-                spd_y_max = (spd_y_max < spd_y) ? spd_y : spd_y_max;
+                /*
+                 * spd_x_min = (spd_x_min > spd_x) ? spd_x : spd_x_min;
+                           spd_x_max = (spd_x_max < spd_x) ? spd_x : spd_x_max;
+                           spd_y_min = (spd_y_min > spd_y) ? spd_y : spd_y_min;
+                           spd_y_max = (spd_y_max < spd_y) ? spd_y : spd_y_max;
 
-                Debug.Log("speed " + spd_x + ", " + spd_y + " | " + spd_x_min + " " + spd_x_max + " " + spd_y_min + " " + spd_y_max);
-                Debug.Log("t.fingerId " + t.fingerId);
-                //    Debug.Log("t.maximumPossiblePressure" + t.maximumPossiblePressure);
-                Debug.Log("t.phase " + t.phase);
-                Debug.Log("t.position " + t.position);
-                //    Debug.Log("t.pressure" + t.pressure);
-                Debug.Log("t.radius " + t.radius);
-                Debug.Log("t.radiusVariance " + t.radiusVariance);
-                Debug.Log("t.rawPosition " + t.rawPosition);
-                Debug.Log("t.tapCount " + t.tapCount);
-                //   Debug.Log("t.type" + t.type);
+                           Debug.Log("speed " + spd_x + ", " + spd_y + " | " + spd_x_min + " " + spd_x_max + " " + spd_y_min + " " + spd_y_max);
+                           Debug.Log("t.fingerId " + t.fingerId);
+                           //    Debug.Log("t.maximumPossiblePressure" + t.maximumPossiblePressure);
+                           Debug.Log("t.phase " + t.phase);
+                           Debug.Log("t.position " + t.position);
+                           //    Debug.Log("t.pressure" + t.pressure);
+                           Debug.Log("t.radius " + t.radius);
+                           Debug.Log("t.radiusVariance " + t.radiusVariance);
+                           Debug.Log("t.rawPosition " + t.rawPosition);
+                           Debug.Log("t.tapCount " + t.tapCount);
+                           //   Debug.Log("t.type" + t.type);
+                */
 
-                Debug.Log("numBalls " + numBalls);
-                if (numBalls < maxNumBalls)
+                if (Mathf.Abs(prev_x) < Mathf.Abs(spd_x) )
                 {
-                    GameObject ball = Instantiate(ballPrefab);
-                    ball.transform.position = new Vector3(1, 1, 0);
-                    Rigidbody rb = ball.GetComponent<Rigidbody>();
-                    Vector3 force = new Vector3(spd_x / 10f, spd_x / 10f, spd_y / 10f);
-                    rb.AddForce(force);
-                    numBalls += 1;
+                    Debug.Log("throughInterval"+throughInterval);
+                    if (Time.time - lastThrough > throughInterval)
+                        {
+
+                        Debug.Log("numBalls " + numBalls);
+                        if (numBalls < maxNumBalls)
+                        {
+                            GameObject ball = Instantiate(ballPrefab);
+                            ball.transform.position = new Vector3(0, 2, 0);
+                            Rigidbody rb = ball.GetComponent<Rigidbody>();
+                            //                    Vector3 force = new Vector3(spd_x / 10f, spd_x / 10f, spd_y / 10f);
+                            Vector3 force = new Vector3(spd_x, spd_x, spd_y);
+                            rb.AddForce(force);
+                            numBalls += 1;
+                        }
+                        prev_x = 0;
+                        lastThrough = Time.time;
+                    }
+                }
+                else
+                {
+                    prev_x = spd_x;
                 }
             }
         }
